e785d6cc4142fa5ff9eb9485d53ba5297dc92fe6
Oleg Tcybikov <batuich@gmail.com>
2024-02-21 20:29:38 +0700

    Ver 1.0: Add OpenGraph

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,29 @@ import Inspect from 'vite-plugin-inspect'
 import { defineConfig } from 'vite'
 import path from 'path'
 import pugPlugin from "vite-plugin-pug"
+import viteImagemin from '@vheemstra/vite-plugin-imagemin'
 
+// The minifiers you want to use:
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminPngQuant from 'imagemin-pngquant'
+import imageminSvgo from 'imagemin-svgo'
+
+// pug options
 const options = { }
 const locals = { name: "My Pug" }
 
 export default defineConfig({
-  plugins: [ Inspect() , pugPlugin(options, locals) ],
+  plugins: [
+		Inspect(),
+		pugPlugin(options, locals),
+		viteImagemin({
+			plugins: {
+				jpg: imageminMozjpeg(),
+				png: imageminPngQuant(),
+				svg: imageminSvgo()
+			},
+		}),
+	],
 	resolve: {
     alias: {
       // Подключаем только базовый функционал Lottie
